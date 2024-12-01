9ab97b4d48b18818e06085a3e63340e893a18fcf
Charlotte <cmjwfeltwell@gmail.com>
2024-12-01 16:24:58 -0600

    Update disassembler.cpp

--- src/disassembler.cpp
+++ src/disassembler.cpp
@@ -1,2 +1,24 @@
 #include "disassembler.h"
 
+/**
+ * Load ROM
+ * load ROM file into vector of 2-byte (16 bit) opcodes
+ */
+std::vector<uint16_t> loadROM(const std::string& romPath) {
+
+}
+
+/**
+ * Disassemble one opcode
+ * disassemble an opcode into a string description
+ */
+std::string disassembleOpcode(uint16_t opcode) {
+
+}
+
+/**
+ * Disassemble a list of opcodes
+ */
+std::vector<std::string> disassembleList(const std::vector<uint16_t>& opcodes) {
+    
+}
