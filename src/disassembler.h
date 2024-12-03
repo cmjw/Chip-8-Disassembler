da68154d12cab67cd99d37aee725db0429b05582
Charlotte <cmjwfeltwell@gmail.com>
2024-12-03 10:37:48 -0600

    restart

--- src/disassembler.h
+++ src/disassembler.h
@@ -1,21 +1,6 @@
 #ifndef DISASSEMBLER_H
 #define DISASSEMBLER_H
 
-#include <cstdint>
-#include <string>
-#include <vector>
 
-#include <fstream>
-#include <iostream>
-
-
-// load ROM file into vector of 2-byte (16 bit) opcodes
-std::vector<uint16_t> loadROM(const std::string& romPath);
-
-// disassemble an opcode into a string description
-std::string disassembleOpcode(uint16_t opcode);
-
-// disassemble a list of opcodes
-std::vector<std::string> disassembleList(const std::vector<uint16_t>& opcodes);
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,7 @@
 #include "disassembler.h"
-#include <iostream>
 
 int main(int argc, char* argv[]) {
-    // handle args
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <path to rom>" << std::endl;
-        return 1;
-    }
-
-    std::string romPath = argv[1];
-
-    try {
-        std::vector<uint16_t> opcodes = loadROM(romPath);
     
-        std::vector<std::string> instructions = disassembleList(opcodes);
-
-        size_t address = 0x200; // start after reserved area
-
-        for (const auto& instr : instructions) {
-            std::cout << std::hex << address << ": " << instr << std::endl;
-            address += 2;
-        }
-
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return 1;
-    }
 
     return 0;
 }
