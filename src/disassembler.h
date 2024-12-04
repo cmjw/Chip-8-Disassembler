3cebf8addd25aa02dc5ba9439a52d602a8256bb5
Charlotte <cmjwfeltwell@gmail.com>
2024-12-03 18:24:54 -0600

    Update disassembler.h

--- src/disassembler.h
+++ src/disassembler.h
@@ -1,6 +1,14 @@
 #ifndef DISASSEMBLER_H
 #define DISASSEMBLER_H
 
+#include <vector>
+#include <string>
 
+class Disassembler {
+public:
+    void disassemble(const std::vector<uint8_t>& buffer);
+private:
+    std::string decodeOpcode(uint16_t opcode);
+};
 
-#endif
+#endif 
